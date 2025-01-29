81152fa2910143943ad0eb7f2537d89e336cd5d9
Nikolay Shevelev <nikoshot@ya.ru>
2025-01-29 07:46:59 +0100

    111

--- info.php
+++ info.php
@@ -1,1 +1,17 @@
-<?php echo "1111" ?>
+<?php
+function displayCurrentTime() {
+    echo "Current time is now 1111: " . date("Y-m-d H:i:s");
+}
+
+function displayOnlyTime() {
+    echo "Current time is 3333: " . date("H:i:s");
+}
+
+function displayCurrentMonth() {
+    echo "Current month is: " . date("F");
+}
+
+displayCurrentTime();
+displayOnlyTime();
+displayCurrentMonth();
+?>
